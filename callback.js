c15e4ecd68b45aff00467b852455d38520ffe430
401 <401@academicos.cesde.edu.co>
2022-03-01 11:24:30 -0500

    se modifica las funciones

--- callback.js
+++ callback.js
@@ -7,7 +7,7 @@
 }
 funcionPrincipal(function(){
     console.log("Soy la segunda funcion")
-})*/
+})
 
 function sumar(numero1,numero2,callback){
     setTimeout(function(){
@@ -19,4 +19,22 @@ sumar(7,5, function(numero1,numero2,suma){
     console.log(`la suma es: ${suma}`)
     let resta=numero1-numero2
     console.log(`la resta es: ${resta}`)
+})*/
+function registrar(nombre,edad,pass,callback){
+    setTimeout(function(){
+        let usuario={
+            nombre:nombre,
+            edad:edad,
+            password:pass
+        }
+        callback(usuario)
+    },3000)
+}
+registrar("Simon",15,"2108fe",function(usuario){
+    if(usuario.edad>=18){
+        console.log("Bienvenido "+usuario.nombre)
+    }
+    else{
+        console.log("Deje de ver porno "+usuario.nombre)
+    }
 })
